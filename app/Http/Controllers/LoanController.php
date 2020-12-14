c819df9001b4bfa53cbeda9bc6003aa6f570d4db
Ikem-Emmanuel <ikem43@gmail.com>
2020-12-14 14:40:49 +0100

    initial commit

---
+++ app/Http/Controllers/LoanController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoanController extends Controller
+{
+    public function getLoan(){
+        dd('It Works');
+    }
+
+    public function getloanStatus(){
+        dd('It Works');
+    }
+}
