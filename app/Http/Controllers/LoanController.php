07b81df4729109b07b5c21b5b9a6ad91465a9908
David Ilenwabor <d.ilenwabor@botosoft.net>
2020-12-18 12:15:20 +0100

    initial commit

--- app/Http/Controllers/LoanController.php
+++ app/Http/Controllers/LoanController.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
 use Illuminate\Http\Redirect;
 use Illuminate\Support\Facades\Validator;
@@ -12,6 +11,7 @@ use App\Applicant;
 class LoanController extends Controller
 {
     public function getEligibilityStatus(Request $request) {
+        //dd($request);
         $validator=Validator::make($request->all(),
         [
         'email' => 'required|email',
@@ -25,26 +25,33 @@ class LoanController extends Controller
         }
 
         $user_id = 5;
+        $user = new Applicant;
 
         $salary=$request->input('salary');
+        //dd($salary);
         $Amount=$request->input('amount');
         $tenure=$request->input('tenure');
         $email=$request->input('email');
+
 
         $last_loanCollectedDate=DB::table('applications')
         ->where('user_id','=',$user_id)
         ->where('status', '=','PAID')
         ->latest('application_id')
         ->value('end_date');
-
+        $user->$request->all();
+        //dd($data);
+        $user->save();
+        dd($user);
 // TO CHECK IF THE USER CAN APPLY FOR A NEW LOAN, ONLY IF THE INITIAL WAS CONCLUDED.
         $max_amount= $salary*$tenure*0.3;
-
+        //dd($max_amount);
         if( Carbon::parse($last_loanCollectedDate)->addDays(90)->isPast()) {
-            //max_amount per month payable
+           //max_amount per month payable
             return($Amount < $max_amount) ? $this->SuccessfulPage() : $this->Unsuccessful();
         }
-        return('you have successfully applied');
+//save the request into the database
+        return('you have successfully applied');git 
     }
 
     public function SuccessfulPage(){
